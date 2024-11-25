626f77956111ef7643475b66e307fa0534c7a849
Justin113D <Justin113D@gmail.com>
2024-11-26 00:23:23 +0100

    Updated dev version

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 bl_info = {
-    "name": "Hedge Engine I/O !! INCORRECTLY INSTALLED !!",
-    "author": "Justin113D",
+    "name": "Hedgehog Engine I/O !! INCORRECTLY INSTALLED !!",
+    "author": "Justin113D, hedge-dev",
     "description": "INCORRECTLY INSTALLED THE ADDON! Make sure to download from the github repositories release section!",
     "version": (0, 0, 0),
-    "blender": (4, 0, 0),
+    "blender": (4, 4, 0),
     "location": "",
     "warning": "",
-    "doc_url": "https://x-hax.github.io/HedgeEngineBlenderIODocs/",
-    "tracker_url": "https://github.com/X-Hax/HedgeEngineBlenderIO/issues/new",
+    "doc_url": "https://hedge-dev.github.io/HedgehogEngineBlenderIO/",
+    "tracker_url": "https://github.com/hedge-dev/HedgehogEngineBlenderIO/issues/new",
     "category": "Import-Export"
 }
 
@@ -15,5 +15,5 @@ def register():
     raise RuntimeError((
         "\n\n!! Incorrectly installed the addon!!"
 
-        "\n\nMake sure to download HEIO from the github repositories release section:"
-        "\nhttps://github.com/X-Hax/HedgeEngineBlenderIO/releases/"))
+        "\n\nMake sure to install HEIO correctly:"
+        "\nhttps://hedge-dev.github.io/HedgehogEngineBlenderIO/getting_started/installation.html"))
