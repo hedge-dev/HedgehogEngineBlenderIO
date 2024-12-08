3ef14f5c75a75af25c828326f97dd1aaf84e078b
Justin113D <Justin113D@gmail.com>
2024-12-08 04:39:58 +0100

    Models now import with normals

--- blender/source/importing/i_mesh.py
+++ blender/source/importing/i_mesh.py
@@ -11,7 +11,7 @@ def process_mesh_data(
     if mesh_data.Vertices.Length == 0:
         return mesh
 
-        ##################################################
+    ##################################################
     # Mesh initialization
 
     vertices = [(x.Position.X, -x.Position.Z, x.Position.Y)
@@ -25,7 +25,7 @@ def process_mesh_data(
             mesh_data.TriangleIndices[i + 2])
         )
 
-    mesh.from_pydata(vertices, [], faces)
+    mesh.from_pydata(vertices, [], faces, shade_flat=False)
 
     ##################################################
     # Material Data
@@ -43,7 +43,7 @@ def process_mesh_data(
         face_index += face_count
 
     ##################################################
-    # Creating attributes
+    # Texture coordinates
 
     for i, uvmap in enumerate(mesh_data.TextureCoordinates):
         uv_layer = mesh.uv_layers.new(
@@ -51,6 +51,9 @@ def process_mesh_data(
 
         for l, uv in enumerate(uvmap):
             uv_layer.uv[l].vector = (uv.X, uv.Y)
+
+    ##################################################
+    # Colors
 
     color_type = None
 
@@ -61,18 +64,59 @@ def process_mesh_data(
         color_type = 'FLOAT_COLOR'
         color_data = mesh_data.FloatColors
 
-    for i, color_set in enumerate(color_data):
-        color_set_name = "Colors"
-        if i > 0:
-            color_set_name += str(i + 1)
+    if color_type is not None:
+        for i, color_set in enumerate(color_data):
+            color_set_name = "Colors"
+            if i > 0:
+                color_set_name += str(i + 1)
 
-        color_attribute = mesh.color_attributes.new(
-            "Color" + (str(i + 1) if i > 0 else ""),
-            color_type,
-            'CORNER')
+            color_attribute = mesh.color_attributes.new(
+                "Color" + (str(i + 1) if i > 0 else ""),
+                color_type,
+                'CORNER')
 
-        for j, color in enumerate(color_set):
-            color_attribute.data[j].color = (
-                color.X, color.Y, color.Z, color.W)
+            for j, color in enumerate(color_set):
+                color_attribute.data[j].color = (
+                    color.X, color.Y, color.Z, color.W)
+
+    ##################################################
+    # Normals
+
+    mesh.normals_split_custom_set([(x.X, -x.Z, x.Y) for x in mesh_data.Normals])
+
+    # Validating the mesh, as for some reason things really like to break
+    mesh.validate()
+
+    edge_loop_map = [([], []) for _ in mesh.edges]
+
+    for polygon in mesh.polygons:
+
+        for i, loop_index in enumerate(polygon.loop_indices):
+            next_loop_index = polygon.loop_indices[i - 2]
+
+            loop = mesh.loops[loop_index]
+
+            edge = mesh.edges[loop.edge_index]
+            edge_map = edge_loop_map[loop.edge_index]
+
+            if edge.vertices[0] == loop.vertex_index:
+                edge_map[0].append(loop_index)
+                edge_map[1].append(next_loop_index)
+            else:
+                edge_map[0].append(next_loop_index)
+                edge_map[1].append(loop_index)
+
+    for edge in (x for x in mesh.edges if x.use_edge_sharp):
+        loop_map = edge_loop_map[edge.index]
+        if len(loop_map[0]) != 2:
+            continue
+
+        nrm0 = mesh.corner_normals[loop_map[0][0]].vector
+        nrm1 = mesh.corner_normals[loop_map[0][1]].vector
+        nrm2 = mesh.corner_normals[loop_map[1][0]].vector
+        nrm3 = mesh.corner_normals[loop_map[1][1]].vector
+
+        if (nrm0 - nrm1).length_squared < 0.01 and (nrm2 - nrm3).length_squared < 0.01:
+            edge.use_edge_sharp = False
 
     return mesh
