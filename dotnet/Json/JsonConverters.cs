23aefde95ee420bfe61a77d003076dc6a3cc6b27
Justin113D <Justin113D@gmail.com>
2025-02-14 13:00:30 +0100

    Model compile debug json no longer includes null values

--- dotnet/Json/JsonConverters.cs
+++ dotnet/Json/JsonConverters.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HEIO.NET.Json
@@ -13,7 +14,10 @@ namespace HEIO.NET.Json
 
         static JsonConverters()
         {
-            Options = new() { WriteIndented = true };
+            Options = new() { 
+                WriteIndented = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
             Options.Converters.Add(new Matrix4x4JsonConverter());
             Options.Converters.Add(new QuaternionJsonConverter());
             Options.Converters.Add(new Vector2JsonConverter());
