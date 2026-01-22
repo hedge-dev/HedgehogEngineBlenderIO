1f55dd7218c6648936bdc934d9cdad438f6f5e7b
Justin113D <Justin113D@gmail.com>
2026-01-22 13:18:46 +0100

    Fixed annoying gpu errors when building extension

--- blender/source/register/tools/__init__.py
+++ blender/source/register/tools/__init__.py
@@ -1,4 +1,7 @@
+import bpy
 from . import collision_primitive_tools
 
 to_register = []
-to_register.extend(collision_primitive_tools.to_register)
+
+if not bpy.app.background:
+    to_register.extend(collision_primitive_tools.to_register)
