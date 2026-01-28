c8eaf16eb2188f2af46d59017bc8f7f4b7d46b60
Justin113D <Justin113D@gmail.com>
2026-01-28 16:46:07 +0100

    Updated dev version to 0.1.122

--- blender/__init__.dev.py
+++ blender/__init__.dev.py
@@ -11,7 +11,7 @@ bl_info = {
     "name": "Hedgehog Engine I/O DEV BUILD",
     "author": "Justin113D, hedge-dev",
     "description": "Import/Exporter for Hedgehog Engine 3D related formats",
-    "version": (0, 1, 121),
+    "version": (0, 1, 122),
     "blender": (4, 5, 0),
     "doc_url": "https://hedge-dev.github.io/HedgehogEngineBlenderIO/",
     "tracker_url": "https://github.com/hedge-dev/HedgehogEngineBlenderIO/issues/new",
